546974d1796670a37c8e6b4731cd19e289507c0c
Gleyder <gleyderh@gmail.com>
2019-01-26 15:11:54 +0100

    added functions of string attributes

--- models/utils.js
+++ models/utils.js
@@ -8,50 +8,103 @@ import Matrix from 'ml-matrix';
  * @return {Array}
  */
 export function separateClasses(X, y) {
-  var features = X.columns;
-  var totalPerClasses = {};
-  for (var i = 0; i < y.length; i++) {
-    if (totalPerClasses[y[i]] === undefined) {
-      totalPerClasses[y[i]] = 0;
-    }
-    totalPerClasses[y[i]]++;
-  }
-  const classes = Object.keys(totalPerClasses).length;
-  var separatedClasses = new Array(classes);
-  var currentIndex = new Array(classes);
+	var features = X.columns;
+	var totalPerClasses = {};
+	for (var i = 0; i < y.length; i++) {
+		if (totalPerClasses[y[i]] === undefined) {
+			totalPerClasses[y[i]] = 0;
+		}
+		totalPerClasses[y[i]]++;
+	}
+	const classes = Object.keys(totalPerClasses).length;
+	var separatedClasses = new Array(classes);
+	var currentIndex = new Array(classes);
 
-   for (i = 0; i < classes; i++) {
-     const totalsKeys = Object.keys(totalPerClasses); 
-      separatedClasses[i] = new Matrix(totalPerClasses[totalsKeys[i]], features);
-      currentIndex[i] = 0;
-    }
+	for (i = 0; i < classes; i++) {
+		const totalsKeys = Object.keys(totalPerClasses);
+		separatedClasses[i] = new Matrix(
+			totalPerClasses[totalsKeys[i]],
+			features,
+		);
+		currentIndex[i] = 0;
+	}
 
-  const classesList = Object.keys(totalPerClasses); 
+	const classesList = Object.keys(totalPerClasses);
 
-  for (i = 0; i < X.rows; i++) {
-        const auxIndex = classesList.indexOf(y[i].toString());
-        // separatedClasses[y[i].toString()][(currentIndex[y[i]]).toString()] = X[i.toString()];
-        // separatedClasses[auxIndex].setRow(currentIndex[auxIndex], X[i.toString()]);
-        separatedClasses[auxIndex][currentIndex[auxIndex]] = X[i.toString()];
-        currentIndex[auxIndex]++;
-      }
+	for (i = 0; i < X.rows; i++) {
+		const auxIndex = classesList.indexOf(y[i].toString());
+		// separatedClasses[y[i].toString()][(currentIndex[y[i]]).toString()] = X[i.toString()];
+		// separatedClasses[auxIndex].setRow(currentIndex[auxIndex], X[i.toString()]);
+		separatedClasses[auxIndex][currentIndex[auxIndex]] = X[i.toString()];
+		currentIndex[auxIndex]++;
+	}
 
-  return separatedClasses;
+	return separatedClasses;
 }
 
 /**
  * @public
- * Function that retuns an array of classes.
+ * Function that returns an array of classes.
  * @param {Array} y - predictions
  * @return {Array} - List of different classes
  */
 export function getClassesList(y) {
-  var totalPerClasses = {};
-  for (var i = 0; i < y.length; i++) {
-    if (totalPerClasses[y[i]] === undefined) {
-      totalPerClasses[y[i]] = 0;
-    }
-    totalPerClasses[y[i]]++;
-  }
-  return Object.keys(totalPerClasses)
+	var totalPerClasses = {};
+	for (var i = 0; i < y.length; i++) {
+		if (totalPerClasses[y[i]] === undefined) {
+			totalPerClasses[y[i]] = 0;
+		}
+		totalPerClasses[y[i]]++;
+	}
+	return Object.keys(totalPerClasses);
+}
+
+/**
+ * @public
+ * Function that returns an object of attributes numerical representation.
+  * @param {Matrix} X - dataset
+ * @return {Object} - Numerical representation object
+ */
+export function getDatasetNumericalRepresentation(X) {
+	const attributesLength = X[0].length;
+	const auxCounter = new Array(attributesLength).fill(0);
+	const datasetRepresentation = {};
+
+	for (let index = 0; index < attributesLength; index++) {
+		const aux = [];
+		datasetRepresentation[index.toString()] = {};
+		X.map((instance) => {
+			if (!aux.includes(instance[index])) {
+				aux.push(instance[index]);
+				datasetRepresentation[index.toString()][
+					instance[index].toString()
+				] =
+					auxCounter[index];
+				auxCounter[index] = auxCounter[index] + 1;
+			}
+		});
+	}
+
+	return datasetRepresentation;
+}
+
+/**
+ * @public
+ * Function that returns a numerical matrix from string
+ * matrix and attributes numerical representation.
+  * @param {Matrix} X - dataset
+  * @param {Object} numericalAttributes - Numerical Attributes representation
+ * @return {Matrix} - Numerical representation matrix
+ */
+export function getNumerialMatrixFromRepresentation(X, numericalAttributes) {
+	const auxMatrix = [];
+	X.map((instance) => {
+		const aux = [];
+		instance.map((attribute, index) => {
+			aux.push(numericalAttributes[index.toString()][attribute]);
+		});
+		auxMatrix.push(aux);
+	});
+
+	return auxMatrix;
 }
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,56 @@
-import express from 'express'
+import express from 'express';
 import { MultinomialNB, GaussianNB } from '../models';
 import Matrix from 'ml-matrix';
-
+import {
+	getDatasetNumericalRepresentation,
+	getNumerialMatrixFromRepresentation,
+} from '../models/utils';
 
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const matrix = new Matrix([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]]);
-  var model = new GaussianNB();
-  model.train(matrix, [1, 1, 1, 2, 2, 2]);
+	const matrix = new Matrix([
+		[ -1, -1 ],
+		[ -2, -1 ],
+		[ -3, -2 ],
+		[ 1, 1 ],
+		[ 2, 1 ],
+		[ 3, 2 ],
+	]);
+	var model = new GaussianNB();
+	model.train(matrix, [ 1, 1, 1, 2, 2, 2 ]);
 
-  const matrixTest = [[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]];
-  var predictions = model.predict(matrixTest);
-  console.log(predictions)
-  res.json(predictions);
+	const matrixTest = [
+		[ -1, -1 ],
+		[ -2, -1 ],
+		[ -3, -2 ],
+		[ 1, 1 ],
+		[ 2, 1 ],
+		[ 3, 2 ],
+	];
+	var predictions = model.predict(matrixTest);
+	console.log(predictions);
+	res.json(predictions);
+});
+
+/* GET home page. */
+router.get('/text', function(req, res, next) {
+	let matrix = [ [ 'calido', 'seco' ], [ 'frio', 'seco' ] ];
+	const attributesRepresentations = getDatasetNumericalRepresentation(matrix);
+	matrix = getNumerialMatrixFromRepresentation(
+		matrix,
+		attributesRepresentations,
+	);
+	var model = new MultinomialNB();
+	model.train(matrix, [ 'verano', 'invierno' ]);
+
+	const matrixTest = getNumerialMatrixFromRepresentation(
+		[ [ 'frio', 'seco' ] ],
+		attributesRepresentations,
+	);
+	var predictions = model.predict(matrixTest);
+	res.json(predictions);
 });
 
 module.exports = router;
