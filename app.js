f25e9cefc77beab31f436ab6152ba5b2704d8488
Gleyder <gleyderh@gmail.com>
2019-01-30 21:06:04 +0100

    metrics endpoint created

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var filesRouter = require('./routes/files');
+var modelRouter = require('./routes/model');
 
 import frontEndUrl from './config/frontEndUrl';
 
@@ -25,45 +26,51 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
+	// Website you wish to allow to connect
+	res.setHeader('Access-Control-Allow-Origin', frontEndUrl);
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', frontEndUrl);
+	// Request methods you wish to allow
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+	);
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	// Request headers you wish to allow
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-Requested-With,content-type',
+	);
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	// Set to true if you need the website to include cookies in the requests sent
+	// to the API (e.g. in case you use sessions)
+	res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
+	// Pass to next layer of middleware
+	next();
 });
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/files', filesRouter);
+app.use('/model', modelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
-app.listen(3001, () => console.log('app listening on port 3001!'))
+app.listen(3001, () => console.log('app listening on port 3001!'));
 
 module.exports = app;
---
+++ routes/model.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { MultinomialNB, GaussianNB } from '../models';
+import Matrix from 'ml-matrix';
+import {
+	getDatasetNumericalRepresentation,
+	getNumerialMatrixFromRepresentation,
+} from '../models/utils/models';
+import { crossValidationModel } from '../models/utils/evaluation';
+
+var router = express.Router();
+
+/* Cross Validation Metrics from data */
+router.post('/multinomial/create/cv', function(req, res, next) {
+	console.log(req);
+	var data = {
+		instances: req.body.data.instances,
+		classes: req.body.data.classes,
+	};
+
+	var metrics = crossValidationModel(
+		MultinomialNB,
+		data.instances,
+		data.classes,
+	);
+	res.json(metrics);
+});
+
+module.exports = router;
