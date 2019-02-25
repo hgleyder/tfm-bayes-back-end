bc51af968c3925d7f2d9d0790efc0102525b5644
Gleyder <gleyderh@gmail.com>
2019-02-26 00:06:35 +0100

    dataset creation added

--- routes/spam.js
+++ routes/spam.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import ErrorMessages from '../utils/errorMessages';
+import fs from 'fs';
 import { loadModelFromImportedData } from '../models/utils/import';
 import {
 	removeStopwordsAndApplyStemmer,
 	readAttributesFromFile,
 	getInstanceFromAttributes,
+	createAttributesFile,
+	createDatasetFile,
 } from '../utils/preprocesing';
 
 var router = express.Router();
@@ -14,16 +17,53 @@ router.post('/predict', function(req, res, next) {
 	var modelData = req.body.modelData;
 	var text = req.body.text;
 	const wordList = removeStopwordsAndApplyStemmer(text);
-	const attributes = readAttributesFromFile('./uploads/attributes.txt');
-	const instance = getInstanceFromAttributes(wordList, attributes);
+	fs.readFile('./uploads/attributes.txt', 'utf8', (err, data) => {
+		if (err) throw err;
+		let attributes = data.split('\n');
+		attributes.splice(attributes.length - 1, 1);
+		const instance = getInstanceFromAttributes(wordList, attributes);
+		try {
+			const model = loadModelFromImportedData(modelData);
+			const predictions = model.predict([ instance ]);
+			res.json(predictions.map((p) => model.classes[parseInt(p)]));
+		} catch (e) {
+			res.status(500).send({ error: ErrorMessages.ERROR_OCCURRED_LOAD });
+		}
+	});
+});
 
-	try {
-		const model = loadModelFromImportedData(modelData);
-		const predictions = model.predict(instance);
-		res.json(predictions.map((p) => model.classes[parseInt(p)]));
-	} catch (e) {
-		res.status(500).send({ error: ErrorMessages.ERROR_OCCURRED_LOAD });
-	}
+/* Load model from json */
+router.get('/predict', function(req, res, next) {
+	var modelData = req.body.modelData;
+	var text =
+		'you just won an increadible prize, please click here to claim your reward!';
+	const wordList = removeStopwordsAndApplyStemmer(text);
+	fs.readFile('./uploads/attributes2.txt', 'utf8', (err, data) => {
+		if (err) throw err;
+		let attributes = data.split('\n');
+		const instance = getInstanceFromAttributes(wordList, attributes);
+		try {
+			res.json(instance);
+		} catch (e) {
+			res.status(500).send({ error: ErrorMessages.ERROR_OCCURRED_LOAD });
+		}
+	});
+});
+
+// create attibutes file from instances data
+router.get('/create-attributes', function(req, res, next) {
+	createAttributesFile('./uploads/spamData.csv', '/---/');
+	res.send('attributes file created');
+});
+
+// create Dataset
+router.get('/create-dataset', function(req, res, next) {
+	createDatasetFile(
+		'./uploads/spamData.csv',
+		'/---/',
+		'./uploads/attributes2.txt',
+	);
+	res.send('dataset file created');
 });
 
 module.exports = router;
--- utils/preprocesing.js
+++ utils/preprocesing.js
@@ -14,7 +14,64 @@ export const readAttributesFromFile = (attributesPath) => {
 	fs.readFile(attributesPath, 'utf8', (err, data) => {
 		if (err) throw err;
 		const attributes = data.split('\n');
+		attributes.splice(attributes.length - 1, 1);
 		return attributes;
+	});
+};
+
+export const createAttributesFile = (instancesPath, separator) => {
+	let words = [];
+	fs.readFile(instancesPath, 'utf8', (err, data) => {
+		if (err) throw err;
+		const instances = data.split('\n');
+		instances.map((i) => {
+			let attributes = i
+				.split(separator)[0]
+				.toLowerCase()
+				.match(/([a-z]+)/g);
+			if (attributes) {
+				attributes = sw.removeStopwords(attributes, sw.en);
+				attributes = attributes.map((w) => stemmer(w));
+				attributes.filter((a) => a.length > 1).map((a) => {
+					if (words.indexOf(a) === -1) {
+						words.push(a);
+					}
+				});
+			}
+		});
+		const data2 = words.join('\n');
+		fs.writeFileSync(`uploads/attributes2.txt`, data2);
+	});
+};
+
+export const createDatasetFile = (instancesPath, separator, attributesPath) => {
+	fs.readFile(attributesPath, 'utf8', (e, attributesData) => {
+		fs.readFile(instancesPath, 'utf8', (err, data) => {
+			if (err) throw err;
+			const instances = data.split('\n');
+			instances.map((i) => {
+				let attributes = i
+					.split(separator)[0]
+					.toLowerCase()
+					.match(/([a-z]+)/g);
+				let clas = i
+					.split(separator)[1]
+					.toLowerCase()
+					.match(/([a-z]+)/g)[0];
+				if (attributes && i.split(separator).length === 2) {
+					attributes = sw.removeStopwords(attributes, sw.en);
+					attributes = attributes.map((w) => stemmer(w));
+					const auxInstance = getInstanceFromAttributes(
+						attributes,
+						attributesData.split('\n'),
+					);
+					fs.appendFileSync(
+						`uploads/dataset.csv`,
+						auxInstance + ',' + clas + '\n',
+					);
+				}
+			});
+		});
 	});
 };
 
