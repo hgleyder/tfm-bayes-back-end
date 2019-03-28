bb4b08406f953301b964d20e24b1fbb34e8d5f56
Gleyder <gleyderh@gmail.com>
2019-03-28 21:09:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -37,23 +37,19 @@ app.use(function(req, res, next) {
 	];
 	var origin = req.headers.origin;
 	if (allowedOrigins.indexOf(origin) > -1) {
-		res.setHeader('Access-Control-Allow-Origin', origin);
 	}
-	// Request methods you wish to allow
+
+	res.setHeader('Access-Control-Allow-Origin', '*');
+
+	res.setHeader('Access-Control-Allow-Credentials', 'true');
 	res.setHeader(
 		'Access-Control-Allow-Methods',
-		'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+		'GET,HEAD,OPTIONS,POST,PUT,DELETE',
 	);
-
-	// Request headers you wish to allow
 	res.setHeader(
 		'Access-Control-Allow-Headers',
-		'X-Requested-With,content-type',
+		'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, access-control-allow-origin',
 	);
-
-	// Set to true if you need the website to include cookies in the requests sent
-	// to the API (e.g. in case you use sessions)
-	res.setHeader('Access-Control-Allow-Credentials', true);
 
 	// Pass to next layer of middleware
 	next();
